e04da911b3d9ac530887bd1685b46aa7b5cc767c
Matheus Montanha <matheusmontanhakk@gmail.com>
2018-11-21 20:58:12 -0200

    component placed in its own repository

---
+++ Source/StructureType.cs
@@ -0,0 +1,12 @@
+namespace Plets.Core.ControlStructure {
+    public enum StructureType {
+        UmlModel,
+        HSI,
+        DFS,
+        TestPlan,
+        None,
+        Wp,
+        OATS,
+        DFS_TCC
+    }
+}
